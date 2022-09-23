f750b902efa5bccd6bdcebde72fdcd7898876898
quantumde <89156762+quantumde@users.noreply.github.com>
2022-09-23 18:38:27 +0300

    Added dirt block

--- src/blocks/generate_engine.cpp
+++ src/blocks/generate_engine.cpp
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <curses.h>
 #include <stdlib.h>
+#include "../structures/Structures.cpp"
 
 static void world_gen(int x, int y)
 {
-    blocks ground = blocks(x, y, "Ground", '%');
-    /*Blocks stone = Blocks(x, y, "Stone", '$');
-    Blocks hardstone = Blocks(x, y, "HardStone", '#');*/
+    blocks ground;
+    ground.name = "Dirt Block";
+    ground.coor_y = y;
+    ground.coor_x = x;
+    ground.blocks_view = "#";
     for (int i = 0; i < x & i < y; i++)
     {
         for (int j = 0; j < y; j++)
         {
             clear();
-            mvprintw(y, x, ground.name);
+            mvprintw(y, x, ground.blocks_view);
             sleep(10);
         }
     }
